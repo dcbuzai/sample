923f64ac1a9726963719711882c9c7a555645563
test <mywin7@qq.com>
2018-08-14 15:19:36 +0000

    8-14 xuexi

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.default')
-@section('title', '主页')
 
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Dc de Laravel</h1>
+        <p class="lead">
+            你现在看到的是<a href="#">我创建的</a>项目主页.
+        </p>
+        <p>
+            一切将开始了哦~
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
