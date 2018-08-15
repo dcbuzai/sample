fbca384131c97521ba30b06ede9fb4c982c77b5f
test <mywin7@qq.com>
2018-08-15 14:02:24 +0000

    finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -10,7 +10,7 @@
             一切将开始了哦~
         </p>
         <p>
-            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
         </p>
     </div>
 @stop
