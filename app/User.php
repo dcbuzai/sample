0c804a641306866168bb5e2f436083be3767d401
test <mywin7@qq.com>
2018-08-26 04:30:45 +0000

    Finish login and logout

---
+++ app/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table = 'users';
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function gravatar($size = '100')
+    {
+        return "https://secure.gravatar.com/avatar/ec22a43e144cbe34c521716f80076b23=$size";
+    }
+}
